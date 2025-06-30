baeb8663de3477ea5bc5537c9b11aed62c07155d
Tung Le <tung.le@advocado.me>
2025-06-30 19:04:50 +0700

    all

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 w-screen bg-gray-100">
